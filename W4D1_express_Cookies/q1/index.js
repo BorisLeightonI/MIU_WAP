a93bb3832e3ecb100517ada11e4c28dd02ef33ef
Boris LeightonI <borisleighton@gmail.com>
2022-05-16 22:48:34 -0400

    W4D1

---
+++ W4D1_express_Cookies/q1/index.js
@@ -0,0 +1,58 @@
+'use strict'
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var app = module.exports = express();
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
+// custom log format
+if (process.env.NODE_ENV !== 'test') app.use(logger(':method :url'))
+
+// parses request cookies, populating
+// req.cookies and req.signedCookies
+// when the secret is passed, used
+// for signing the cookies.
+app.use(cookieParser('salt for cooking'))
+
+// parses x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.static('views'))
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+
+  res.render('form')
+  // if (req.cookies.remember) {
+  //   res.send('Remembered :). Click to <a href="/forget">forget</a>!.');
+  // } else {
+  //   res.send('<form method="post"><p>Check to <label>'
+  //     + '<input type="checkbox" name="remember"/> remember me</label> '
+  //     + '<input type="submit" value="Submit"/>.</p></form>');
+  // }
+});
+
+app.post('/', (req, res)=>{
+
+});
+
+app.get('/forget', function(req, res){
+  res.clearCookie('remember');
+  res.redirect('back');
+});
+
+// app.post('/', function(req, res){
+//   var minute = 60000;
+//   if (req.body.remember) res.cookie('remember', 1, { maxAge: minute });
+//   res.redirect('back');
+// });
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}

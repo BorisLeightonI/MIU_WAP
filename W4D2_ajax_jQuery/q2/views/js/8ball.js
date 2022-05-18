22427c7f8de4abcf1153a785cd3971de3c0c012c
Boris LeightonI <borisleighton@gmail.com>
2022-05-18 00:08:39 -0400

    positioning answers repo

--- W4D2_ajax_jQuery/q2/views/js/8ball.js
+++ W4D2_ajax_jQuery/q2/views/js/8ball.js
@@ -1,8 +1,4 @@
-const answers = [ "It is Certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes",
-"Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later",
-"Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it",
-"My reply is no", "My sources say no", "Outlook not so good", "Very doubtful" ];
-console.log(answers.length);
+
 
 $('#8ball').submit(()=>{
     $('#text').val('');
@@ -11,6 +7,7 @@ $('#8ball').submit(()=>{
         url: '/test'
     }).done((data)=>{
         console.log(data);
+        $('p').text(data.title);
     }).fail((e)=>{
         console.error(e);
     });
